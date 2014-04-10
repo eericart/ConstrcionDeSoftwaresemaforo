081d0d22162e0724e81fceacd77534d004bea4fa
Daniel Chan <daniel.chan.valette@gmail.com>
2014-04-10 18:58:29 -0400

    Indentation fix

--- Test calle.py
+++ Test calle.py
@@ -9,19 +9,20 @@ import intercesion
 class Street(object):
   """docstring for Street"""
   def __init__(self, x,y,rails,directions):
-    super(Street, self).__init__()
-    self.x = x
-    self.y = y
-    self.rails = rails
-    self.intercessiones = []
-    self.directions = directions
-    self.cars = []
+	super(Street, self).__init__()
+	self.x = x
+	self.y = y
+	self.rails = rails
+	self.intercessiones = []
+	self.directions = directions
+	self.cars = []
 
-    car = []
-    for x in range(10):
-        car[x] = vehiculo.Car(1000,220,direccion.Directions().east,self.intercessiones[0])
-        print 'Creado el ', car[x], 'carro'
-        self.cars.append(car)
+  	def carGenerator(waitTime):
+		car = []
+		for x in range(10):
+			car[x] = vehiculo.Car(1000,220,direccion.Directions().east,self.intercessiones[0])
+			print 'Creado el ', car[x], 'carro'
+			self.cars.append(car)
 
     #def carGenerator(tiempoEspera):
 	
